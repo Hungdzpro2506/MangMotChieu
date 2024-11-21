2c9e9b1b0978c1b5a14c53d8eed0b659e06f6e25
Hungdzpro2506 <134621730+Hungdzpro2506@users.noreply.github.com>
2024-11-21 15:44:01 +0700

    Add TinhTong file

---
+++ TinhTong/ConsoleApp1/ConsoleApp1/TinhTong.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class TinhTong
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nhap so luong phan tu cua mang");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] arr=new int[n];
+
+            Console.WriteLine("Nhap gia tri cho mang: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Phan tu thu {i+1} : ");
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+            int Tong = 0;
+            for (int i = 0;i < arr.Length; i++)
+            {
+                Tong+=arr[i];
+            }
+            Console.WriteLine("Tong cac phan tu trong mang la:"+ Tong);
+        }
+    }
+}
